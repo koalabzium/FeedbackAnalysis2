556b567c5cf2ade829965c22a63e5b0d7de0774b
koalabzium <zosia.owsiak@gmail.com>
2018-10-17 16:48:06 +0200

    find_most_popular_airline handles more than one most popular

--- feedback_analysis.py
+++ feedback_analysis.py
@@ -1,7 +1,5 @@
-# zgarnij wszystko w klasę
-# rób tylko na dictionaries
-#  OGARNIJ LEPIEJ CZYTANIE Z CSV... BO OBCIACH TROCHE NO ALE NO...
 # POMYŚL JAK ŁADNIE PRZEDTSAWIĆ DYSTRYBUCJĘ
+    # wwyawal breaka ziom
 
 import csv
 import json
@@ -11,17 +9,23 @@ class Feedback:
 
     def __init__(self, file_path):
         if file_path.lower().endswith(".json"):
-            self.feedback = Feedback.read_json(file_path)
+            feedback = Feedback.read_json(file_path)
 
         elif file_path.lower().endswith(".csv"):
-            self.feedback = Feedback.read_csv(file_path)
+            feedback = Feedback.read_csv(file_path)
 
         else:
             raise ValueError("This file format is not supported yet.")
 
+        self.feedback = feedback
+
     def distribution_fellow_passengers(self):
         number_of_fellow_passengers = self.feedback["number_of_fellow_passengers"]
-        distribution = dict((str(x) + " passengers have", str(number_of_fellow_passengers.count(x)) + " fellow passengers") for x in set(number_of_fellow_passengers))
+        distribution = [(int(x), number_of_fellow_passengers.count(x)) for x in set(number_of_fellow_passengers)]
+
+        for el, key in sorted(distribution):
+            print("Having " + str(el) + " fellow passengers has occurred " + str(key) + " times.")
+
         return distribution
 
     def calculate_average_compensation_per_passenger(self):
@@ -34,8 +38,21 @@ class Feedback:
             raise ZeroDivisionError("There are no passengers that got the compensation")
         return sum(compensation_per_passenger) / len(compensation_per_passenger)
 
-    def calculate_most_popular_airline(self):
-        return max(set(self.feedback["airline_code"]), key=self.feedback["airline_code"].count)
+    def find_most_popular_airline(self):
+        airline_code = self.feedback["airline_code"]
+        result = []
+        if len(airline_code) == 0:
+            raise ValueError("airline_code column is empty")
+        airline_summary = [(x, airline_code.count(x)) for x in set(airline_code)]
+        airline_summary = sorted(airline_summary, key= lambda x: -x[1])
+        max_value = airline_summary[0][1]
+        for k, v in airline_summary:
+            if v == max_value:
+                result.append(k)
+            else:
+                break
+
+        return set(result)
 
     def calculate_got_compensation_percentage(self):
         count_receive = 0
@@ -47,18 +64,21 @@ class Feedback:
             raise ZeroDivisionError("Did_receive_compensation column is empty.")
         return count_receive / len(self.feedback["did_receive_compensation"]) * 100
 
+    def extract_messages(self):
+        return self.feedback["message"]
+
     @staticmethod
     def read_csv(file_path):
-        with open(file_path, newline='') as csv_file:
+        with open(file_path) as csv_file:
             reader = csv.DictReader(csv_file)
             data = dict([('message', []), ('airline_code', []), ('number_of_fellow_passengers', []),
                          ('did_receive_compensation', []), ('total_compensation_amount', [])])
             for row in reader:
-                data['message'].append(row['message'])
-                data['airline_code'].append(row['airline_code'])
-                data['number_of_fellow_passengers'].append(row['number_of_fellow_passengers'])
-                data['did_receive_compensation'].append(row['did_receive_compensation'])
-                data['total_compensation_amount'].append(row['total_compensation_amount'])
+                for key, el in row.items():
+                    if key == 'message' or key == "airline_code":
+                        data[key].append(el)
+                    else:
+                        data[key].append(int(el))
 
         return data
 
@@ -73,4 +93,5 @@ class Feedback:
 
 
 f = Feedback('testowy.json')
-print(f.distribution_fellow_passengers())
+print(f.find_most_popular_airline())
+
